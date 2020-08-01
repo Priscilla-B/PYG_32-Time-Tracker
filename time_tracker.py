36679e4977503b8fc077432b82831db89f108a4e
Priscilla Baah <pyfbaah@gmail.com>
2020-08-01 06:42:31 +0000

    includes comments and functions to make code more readable

--- time_tracker.py
+++ time_tracker.py
@@ -11,48 +11,77 @@ import time
 import csv
 # todo: write a code that excludes the time the person takes to enter the start/stop if they made a mistake initially
 # todo: convert time spent to two decimal places
-# todo: create spaces and some kind of waiting message for user to finish project
-# todo: add messages that tell user the time they started and the time they finished project
-# todo: add comments to make code more understandable
 # todo: import decimals to format floats to two decimal places
-# todo: include comments to make code readable
 # todo: include in readme that csv file should not be open in excel while running the code
+# todo: include in readme that this program requires a python ide or text editor or the python idle to run
+
+PAY_PER_HOUR = 5  # constant
 
 
 def main():
-    data = {}
+    data = {}  # empty dictionary to collect data on the project
+    get_data(data)
+    save_data(data)
+
+
+def get_data(data):
+    """This function collects both input and output data on the project and saves the data into a dictionary.
+        Input data is collected by telling user to enter project name and a 'start' or 'stop' prompt to start
+        or stop a project respectively. Output data is collected on the time spent on project and the amount earned
+        within that time.
+    """
+
     project_name = input("What is the name of your project?")
-    data["Project_Name"] = project_name
-    start_time = input("Enter 'Start' to begin time tracker")
-    while start_time != "Start":
+    data["Project_Name"] = project_name  # append project name to dictionary
+    start_time = input("\nEnter 'Start' to begin time tracker")
+
+    while start_time != "Start":  # ensures program doesn't break if user doesn't enter the 'Start'correctly
         start_time = input("Remember, 'Start' is case-sensitive")
-    start_time = time.time()
-    print(start_time)
-    print(time.ctime(start_time))
-    data["Start_Time"] = time.ctime(start_time)
+    start_time = time.time()  # returns time project started in seconds
+    start_time_date_format = time.ctime(start_time)  # converts time in seconds to date format
+    print("\nYou started work at "+start_time_date_format+" prompt.")  #
+    data["Start_Time"] = time.ctime(start_time)  # append time started to dictionary
+
+    print("\nWaiting to finish project....\n")
 
     stop_time = input("Enter 'Stop' to stop time tracker")
-    while stop_time != "Stop":
+    while stop_time != "Stop":  # ensures program doesn't break if user doesn't enter the 'Stop'correctly
         stop_time = input("Remember, 'Stop' is case-sensitive")
     stop_time = time.time()
-    print(stop_time)
-    print(time.ctime(stop_time))
-    data["Stop_Time"] = time.ctime(stop_time)
+    stop_time_date_format = time.ctime(stop_time)
+    print("\nYou finished your project at "+str(stop_time_date_format))
+    data["Stop_Time"] = time.ctime(stop_time)  # append time stopped to dictionary
 
+    # divides time spent(in secs) by number of secs in in hour to get hour equivalent
     time_spent = (stop_time - start_time) / 3600
-    print(time_spent)
-    data["Time_Spent(hours)"] = time_spent
+    print("\nAwesome! You spent "+str(time_spent)+" hours on the project")  # tells user time spent in hours
+    data["Time_Spent(hours)"] = time_spent  # append time spent to dictionary
 
-    amount_earned = 5 * time_spent
-    print("Congratulations! You earned $ " + str(amount_earned) + " in " + str(time_spent) + " hours.")
-    data["Amount_Earned($)"] = amount_earned
+    # calculating amount earned on project
+    amount_earned = PAY_PER_HOUR * time_spent
+    print("\nCongratulations! You earned $ " + str(amount_earned) + " in " + str(time_spent) + " hours.")
+    data["Amount_Earned($)"] = amount_earned  # append amount earned to dictionary
+    return data  # returns data collected on the project for saving purpose
 
+
+def save_data(data):
+    """This function takes in a dictionary containing data from the program(both input and output data)
+        When it gets the data, it saves it as a csv file. In saving the data as a csv file, the function makes sure
+        that new data does not write over old data every time the program runs. It also ensures that fieldnames are not
+        repeated every time new data is collected.
+    """
+
+    # creating csv file
     with open("time_tracker_data.csv", "a", newline="") as outfile:
         fieldnames = ["Project_Name", "Start_Time", "Stop_Time", "Time_Spent(hours)", "Amount_Earned($)"]
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+        # reading csv file to extract fieldnames
         with open("time_tracker_data.csv", "r")as reader:
             read = csv.DictReader(reader)
             headers = read.fieldnames
+
+        # ensures headers are not repeated if there are already headers present
         if headers == None:
             writer.writeheader()
             writer.writerow(data)
