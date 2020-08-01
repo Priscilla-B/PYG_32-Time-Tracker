9a56af30bdf1ecedfb2360fa7a5225fdc9fbc275
Priscilla Baah <pyfbaah@gmail.com>
2020-08-01 09:28:34 +0000

    adds feature to calculate amount earned on an old project

--- time_tracker.py
+++ time_tracker.py
@@ -11,6 +11,8 @@ import time
 import csv
 # todo: include in readme that csv file should not be open in excel while running the code
 # todo: include in readme that this program requires a python ide or text editor or the python idle to run
+# todo: when entering date, make date project starts optional
+# todo: include a welcome message
 
 PAY_PER_HOUR = 5  # constant(in dollars)
 
@@ -22,32 +24,53 @@ def main():
 
 
 def get_data(data):
+
     """This function collects both input and output data on the project and saves the data into a dictionary.
         Input data is collected by telling user to enter project name and a 'start' or 'stop' prompt to start
         or stop a project respectively. Output data is collected on the time spent on project and the amount earned
         within that time.
     """
+    project_type = input("Working on a new project?")
+    if project_type == "Yes":
+        project_name = input("What is the name of your project?")
+        data["Project_Name"] = project_name  # append project name to dictionary
+        start_time = input("\nEnter 'Start' to begin time tracker")
 
-    project_name = input("What is the name of your project?")
-    data["Project_Name"] = project_name  # append project name to dictionary
-    start_time = input("\nEnter 'Start' to begin time tracker")
+        while start_time != "Start":  # ensures program doesn't break if user doesn't enter the 'Start'correctly
+            start_time = input("Remember, 'Start' is case-sensitive")
+        start_time = time.time()  # returns time project started in seconds
+        start_time_date_format = time.ctime(start_time)  # converts time in seconds to date format
+        print("\nYou started work at "+start_time_date_format+" prompt.")  #
+        data["Start_Time"] = time.ctime(start_time)  # append time started to dictionary
 
-    while start_time != "Start":  # ensures program doesn't break if user doesn't enter the 'Start'correctly
-        start_time = input("Remember, 'Start' is case-sensitive")
-    start_time = time.time()  # returns time project started in seconds
-    start_time_date_format = time.ctime(start_time)  # converts time in seconds to date format
-    print("\nYou started work at "+start_time_date_format+" prompt.")  #
-    data["Start_Time"] = time.ctime(start_time)  # append time started to dictionary
+        print("\nWaiting to finish project....\n")
 
-    print("\nWaiting to finish project....\n")
+        stop_time = input("Enter 'Stop' to stop time tracker")
+        while stop_time != "Stop":  # ensures program doesn't break if user doesn't enter the 'Stop'correctly
+            stop_time = input("Remember, 'Stop' is case-sensitive")
+        stop_time = time.time()
+        stop_time_date_format = time.ctime(stop_time)
+        print("\nYou finished your project at " + str(stop_time_date_format))
+        data["Stop_Time"] = time.ctime(stop_time)  # append time stopped to dictionary
 
-    stop_time = input("Enter 'Stop' to stop time tracker")
-    while stop_time != "Stop":  # ensures program doesn't break if user doesn't enter the 'Stop'correctly
-        stop_time = input("Remember, 'Stop' is case-sensitive")
-    stop_time = time.time()
-    stop_time_date_format = time.ctime(stop_time)
-    print("\nYou finished your project at "+str(stop_time_date_format))
-    data["Stop_Time"] = time.ctime(stop_time)  # append time stopped to dictionary
+    if project_type == "No":
+        project_name = input("What is the name of the project?")
+        data["Project_Name"] = project_name  # append project name to dictionary
+        #start_date = input("Enter date project starts")
+        start_hour = int(input("Enter hour project starts"))
+        start_hour_secs = start_hour*3600
+        start_minute = int(input("Enter minute project starts"))
+        start_minute_secs = start_minute*60
+        start_time = start_hour_secs + start_minute_secs
+        #data["Start Time"] = str(start_time)
+
+        stop_hour = float(input("Enter hour project ends"))
+        stop_hour_secs = stop_hour * 3600
+        stop_minute = float(input("Enter minute project ends"))
+        stop_minute_secs = stop_minute * 60
+        stop_time = stop_hour_secs + stop_minute_secs
+        #data["Stop Time"] = str(stop_time)
+
 
     # divides time spent(in secs) by number of secs in in hour to get hour equivalent
     time_spent = round((stop_time - start_time) / 3600, 2)
@@ -88,5 +111,3 @@ def save_data(data):
 
 if __name__ == '__main__':
     main()
-
-
